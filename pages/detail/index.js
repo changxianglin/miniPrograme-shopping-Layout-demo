aaa2fa9e2b029a31d001cff546fe9d6bf3b6b819
changxianglin <281532612@qq.com>
2020-03-27 01:08:21 +0800

    detail layout goods

---
+++ pages/detail/index.js
@@ -0,0 +1,166 @@
+// pages/detail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiper: [
+      {
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585247517804&di=e3b225ffc7f77f318fb559ada5f00ab2&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20171129%2Fd26754ffd2d04c15b802c7a8ccbe806d.jpeg',
+        name: '',
+      },
+      {
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585247517804&di=e3b225ffc7f77f318fb559ada5f00ab2&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20171129%2Fd26754ffd2d04c15b802c7a8ccbe806d.jpeg',
+        name: '',
+      },
+      {
+        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585247517804&di=e3b225ffc7f77f318fb559ada5f00ab2&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20171129%2Fd26754ffd2d04c15b802c7a8ccbe806d.jpeg',
+        name: '',
+      }
+    ],
+    detail: '',
+    list: [
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+      {
+        name: 'v站三'
+      },
+      {
+        name: '李斯特'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
